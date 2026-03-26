b198fa4956e52fd1432b13c571a2254a6dc43ded
Benjamin <gabonnie1234@gmail.com>
2026-03-26 06:16:03 -0400

    chore: flag for bench in test

--- test.sh
+++ test.sh
@@ -7,10 +7,12 @@ cargo test
 echo "Testing but parallel..."
 cargo test --features parallel
 
-echo "Running benchmarks..."
-cargo bench
+[[ "$*" == *--bench* ]] && {
+	echo "Running benchmarks..."
+	cargo bench
 
-echo "Running benchmarks but parallel..."
-cargo bench --features parallel
+	echo "Running benchmarks but parallel..."
+	cargo bench --features parallel
+}
 
 echo "Looks good! I think."
