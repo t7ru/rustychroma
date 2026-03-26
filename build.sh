50f52eaf61a02ae517e0d996b1936add8a377511
Benjamin <gabonnie1234@gmail.com>
2026-03-26 05:50:31 -0400

    chore: parallel for c, dylib mac

--- build.sh
+++ build.sh
@@ -8,11 +8,12 @@ mkdir -p dist/web dist/native
 echo "Building..."
 cargo build --target wasm32-unknown-unknown -r --features wasm
 wasm-bindgen --target web --out-dir dist/web --no-typescript target/wasm32-unknown-unknown/release/rustychroma.wasm
-cargo build --release
+cargo build --release --features c-api,parallel
 cbindgen -o dist/native/rustychroma.h
 
 echo "Organizing..."
 cp target/release/rustychroma.dll dist/native/ 2>/dev/null ||
-cp target/release/librustychroma.so dist/native/
+cp target/release/librustychroma.so dist/native/ 2>/dev/null ||
+cp target/release/librustychroma.dylib dist/native/ 2>/dev/null || true
 
 echo "Heeho! All done!"
