351620979e2dbdafe1be42eec788a9d5cd2e20d2
Benjamin <gabonnie1234@gmail.com>
2026-03-26 05:38:16 -0400

    chore: update test script

--- test.sh
+++ test.sh
@@ -10,4 +10,7 @@ cargo test --features parallel
 echo "Running benchmarks..."
 cargo bench
 
+echo "Running benchmarks but parallel..."
+cargo bench --features parallel
+
 echo "Looks good! I think."
